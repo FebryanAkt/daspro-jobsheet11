afe6ceb1e567605c18939f3c74a88cab1f063ed2
Febryan.Akt <febryan08022006@gmail.com>
2024-11-20 07:42:28 +0700

    Percobaan 1 dan Percobaan 2

--- Kafe10.java
+++ Kafe10.java
@@ -1,5 +1,11 @@
 public class Kafe10 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - RP 15,000");
         System.out.println("2. Cappuccino - RP 20,000");
@@ -11,6 +17,6 @@ public class Kafe10 {
         System.out.println("Silahkan pilih menu yang Anda inginkan.");        
     }
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
 }
