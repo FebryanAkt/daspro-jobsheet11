1d42510605ba73772ea29616791ce129aa9062bc
Febryan.Akt <febryan08022006@gmail.com>
2024-11-20 08:35:49 +0700

    Jawaban pertanyaan nomer 2

--- Kafe10.java
+++ Kafe10.java
@@ -1,11 +1,5 @@
 public class Kafe10 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
-        System.out.println("Selamat datang, " + namaPelanggan + "!");
-
-        if (isMember) {
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        }
-
+    public static void main (String [] args) {
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - RP 15,000");
         System.out.println("2. Cappuccino - RP 20,000");
@@ -15,8 +9,5 @@ public class Kafe10 {
         System.out.println("6. Mie Goreng - RP 18,000");
         System.out.println("=========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");        
-    }
-    public static void main(String[] args) {
-        Menu("Andi", true);
     }
 }
